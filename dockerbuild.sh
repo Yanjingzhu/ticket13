176c24d8c422c7011864acd7351896b0995b812c
weide-zhou <weide-zhou@outlook.com>
2020-03-16 11:43:28 +0800

    Update dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,6 +1,6 @@
 export GA_DOCKER_IMAGE="repo-here:tag1"
-echo tag=v1
-echo value=v3
+echo tag1=v1
+echo tag2=v2
 echo $GA_DOCKER_IMAGE
 #echo "::set-env name=GA_DOCKER_IMAGE::repo-here:tag2"
 #echo "::set-output name=GA_DOCKER_IMAGE4::repo-here:tag4"
