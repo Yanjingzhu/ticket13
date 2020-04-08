c8772f7876323157c130005ccc78cf916591e2b1
weide-zhou <weide-zhou@outlook.com>
2020-04-08 15:22:48 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/sh
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
