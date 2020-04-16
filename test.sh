1d9752b5183e902079e901dfc7265c60e603aa4d
weide-zhou <weide-zhou@outlook.com>
2020-04-16 15:30:37 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
-jobcount=$(curl -X GET https://api.github.com/repos/${{ github.repository }}/actions/runs/${{ github.run_id }}/jobs | jq .total_count)
+#jobcount=$(curl -X GET https://api.github.com/repos/${{ github.repository }}/actions/runs/${{ github.run_id }}/jobs | jq .total_count)
+jobcount=teststring
 echo "::add-mask::$jobcount"
