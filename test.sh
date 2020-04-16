352c3226a2c0da22b1adf89b97a740624e0cc9b0
weide-zhou <weide-zhou@outlook.com>
2020-04-16 15:27:20 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-echo "::add-mask::${{ steps.faketoken.outputs.PAT }}"
+jobcount=$(curl -X GET https://api.github.com/repos/${{ github.repository }}/actions/runs/${{ github.run_id }}/jobs | jq .total_count)
+echo "::add-mask::$jobcount"
